64b38141369896fbf9544ca805e17d0078912643
Guada <romero.guadalupe.epet20@gmail.com>
2026-07-20 20:53:49 -0300

    modifico clase habitacion

--- clases/Habitacion.java
+++ clases/Habitacion.java
@@ -8,57 +8,64 @@ public class Habitacion implements Comparable<Habitacion> {
     private boolean tieneSalida;
     private ArbolAVL desafios; // cada habitacion conoce sus desafios 
 
-    public Habitacion(int codigo) {
-        this.codigo = codigo;
+    //constructores
+    public Habitacion(int unCodigo) {
+        this.codigo = unCodigo;
         this.desafios = new ArbolAVL(); 
     }
 
-    public Habitacion(int codigo, String nombre, int planta, int metros, boolean tieneSalida) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.planta = planta;
-        this.metros = metros;
-        this.tieneSalida = tieneSalida;
+    public Habitacion(int unCodigo, String nom, int unaPlanta, int losMetros, boolean siTieneSalida) {
+        this.codigo = unCodigo;
+        this.nombre = nom;
+        this.planta = unaPlanta;
+        this.metros = losMetros;
+        this.tieneSalida = siTieneSalida;
         this.desafios = new ArbolAVL(); 
     }
 
-    // Métodos observadores (Getters)
-    public int getCodigo() { return codigo; }
-    public String getNombre() { return nombre; }
-    public int getPlanta() { return planta; }
-    public int getMetros() { return metros; }
-    public boolean tieneSalida() { return tieneSalida; }
-    public ArbolAVL getDesafios() { return desafios; }
+    // visualizadores
+    public int getCodigo() {
+        return this.codigo;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public int getPlanta() {
+        return this.planta;
+    }
+    public int getMetros() {
+        return this.metros;
+    }
+    public boolean tieneSalida() {
+        return this.tieneSalida;
+    }
+    public ArbolAVL getDesafios() {
+        return this.desafios;
+    }
 
-    // Métodos modificadores (Setters)
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public void setPlanta(int planta) { this.planta = planta; }
-    public void setMetros(int metros) { this.metros = metros; }
-    public void setTieneSalida(boolean tieneSalida) { this.tieneSalida = tieneSalida; }
+    // modificadores
+    public void setNombre(String nom) {
+        this.nombre = nom;
+    }
+    public void setPlanta(int unaPlanta) {
+        this.planta = unaPlanta;
+    }
+    public void setMetros(int losMetros) {
+        this.metros = losMetros;
+    }
+    public void setTieneSalida(boolean siTieneSalida) {
+        this.tieneSalida = siTieneSalida;
+    }
 
     @Override
     public int compareTo(Habitacion otra) { 
         return Integer.compare(this.codigo, otra.codigo);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        boolean iguales = false;
-        if (obj != null && obj instanceof Habitacion) { 
-            Habitacion otro = (Habitacion) obj; 
-            if (this.codigo == otro.codigo) {
-                iguales = true;
-            }
-        }
-        return iguales;
+    public boolean equals(Habitacion otra) {
+        return this.codigo==otra.codigo;
     }
 
-    @Override
-    public int hashCode() {
-        return Integer.hashCode(this.codigo);
-    }
-
-    @Override
     public String toString() {
         return "Habitacion [Codigo: " + codigo + ", Nombre: " + nombre + ", Planta: " + planta +
                 ", Metros: " + metros + ", Salida Exterior: " + (tieneSalida ? "Sí" : "No") + "]";
