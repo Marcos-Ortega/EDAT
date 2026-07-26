fe1113b983d2d36df7a5dbe56e1499f4cb8178b6
Marcos-Ortega <marcosortega977@gmail.com>
2026-07-26 19:10:50 -0300

    Agrego punto 1 y 2, falta terminar punto 2

---
+++ Sistema/ABM.java
@@ -0,0 +1,595 @@
+package Sistema;
+
+import java.util.Scanner;
+
+import GrafoHabitaciones.estructurasAuxiliares.*;
+import GrafoHabitaciones.Grafo;
+import clases.*;
+import clases.TablaHashEquipos;
+
+public class ABM {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArbolAVL avlHabitaciones = new ArbolAVL();
+        Grafo grafo = new Grafo();
+        TablaHashEquipos tablaEquipos = new TablaHashEquipos();
+        DesafiosResueltos desResueltos = new DesafiosResueltos(); //hacer cuando terminen desafios
+
+        int opc;
+        CargarArchivo.cargarArchivo(avlHabitaciones, grafo, tablaEquipos, desResueltos);
+        do {
+            System.out.println("1. ABM Habitaciones");
+            System.out.println("2. ABM Desafios");
+            System.out.println("3. ABM Equipos");
+            System.out.println("4. Consultas Habitaciones");
+            System.out.println("5. Consultas Desafios");
+            System.out.println("6. Consultas Equipos");
+            System.out.println("7. Mostrar Sistema");
+            System.out.println("0. Salir");
+            System.out.print("Opcion: ");
+
+            opc = sc.nextInt();
+
+            switch (opc) {
+                case 1:
+                    menuHabitaciones(sc, avlHabitaciones, grafo);
+                    break;
+
+                case 2:
+                    menuDesafios(sc, avlHabitaciones);
+                    break;
+
+                case 3:
+                    menuEquipos(sc, tablaEquipos, avlHabitaciones);
+                    break;
+
+                case 4:
+                    //Consulta habitaciones
+                    break;
+
+                case 5:
+                    //Consulta desafíos
+                    break;
+
+                case 6:
+                    //Consulta equipos
+                    break;
+
+                case 7:
+                    System.out.println(avlHabitaciones);
+                    System.out.println(grafo);
+                    System.out.println(tablaEquipos);
+                    System.out.println(desResueltos);
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo..");
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta.");
+
+            }
+
+        } while (opc != 0);
+
+        sc.close();
+    }
+
+    // Menu Habitaciones
+    public static void menuHabitaciones(Scanner sc, ArbolAVL avl, Grafo grafo) {
+
+        int op;
+        int codigoBuscar;
+        Habitacion habActual;
+        int codigo, planta, metros;
+        String nombre, tiene;
+        boolean tieneSalida;
+        do {
+
+            System.out.println("\n- ABM HABITACIONES -");
+            System.out.println("1. Alta habitacion");
+            System.out.println("2. Baja habitacion");
+            System.out.println("3. Modificacion habitacion");
+            System.out.println("0. Volver");
+            System.out.print("Opcion: ");
+
+            op = sc.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    altaHabitacion(sc, avl, grafo);
+                    break;
+
+                case 2:
+                    bajaHabitacion(sc, avl, grafo);
+                    break;
+
+                case 3:
+                    modificarHabitacion(sc, avl);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta.");
+            }
+
+        } while (op != 0);
+
+    }
+
+    public static void altaHabitacion(Scanner sc, ArbolAVL avl, Grafo grafo) {
+
+        int codigo, planta, metros;
+        String nombre;
+        String tiene;
+        boolean tieneSalida = false;
+
+        System.out.println("Ingrese el codigo de la Habitacion: ");
+        codigo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Ingrese el nombre de la Habitacion: ");
+        nombre = sc.nextLine();
+
+        System.out.println("Ingrese la planta de la Habitacion: ");
+        planta = sc.nextInt();
+
+        System.out.println("Ingrese los metros cuadrados de la Habitacion: ");
+        metros = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Tiene salida al exterior? (Si/No): ");
+        tiene = sc.nextLine();
+
+        if (tiene.equalsIgnoreCase("si")) {
+            tieneSalida = true;
+        }
+
+        Habitacion nuevaHabitacion = new Habitacion(codigo, nombre, planta, metros, tieneSalida);
+
+        if (avl.insertar(nuevaHabitacion)) {
+            grafo.insertarHabitacion(codigo);
+            System.out.println("Habitacion agregada exitosamente.");
+        } else {
+            System.out.println("Ya existe una habitacion con ese codigo.");
+        }
+    }
+
+    public static void bajaHabitacion(Scanner sc, ArbolAVL avl, Grafo grafo) {
+
+        int codigoBuscar;
+
+        System.out.println("Ingrese el codigo de la habitacion: ");
+        codigoBuscar = sc.nextInt();
+
+        Habitacion habActual = (Habitacion) avl.recuperar(new Habitacion(codigoBuscar));
+
+        if (habActual != null) {
+            if (habActual.tieneSalida()) {// preguntar con el grupo que pasa ocn una habitacion de entrada
+                System.out.println("No se puede eliminar esta habitacion.");
+            } else {
+                avl.eliminar(habActual);
+                grafo.eliminarHabitacion(codigoBuscar);
+                System.out.println("Habitacion eliminada correctamente.");
+            }
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    public static void modificarHabitacion(Scanner sc, ArbolAVL avl) {
+
+        int codigoBuscar;
+
+        System.out.println("Ingrese el codigo de la habitacion: ");
+        codigoBuscar = sc.nextInt();
+
+        Habitacion habActual = (Habitacion) avl.recuperar(new Habitacion(codigoBuscar));
+
+        if (habActual != null) {
+
+            int opcMod;
+
+            do {
+
+                System.out.println("\n--- MODIFICAR HABITACION ---");
+                System.out.println("1. Modificar Nombre");
+                System.out.println("2. Modificar Planta");
+                System.out.println("3. Modificar Metros");
+                System.out.println("4. Modificar Salida");
+                System.out.println("0. Volver");
+                System.out.print("Opcion: ");
+
+                opcMod = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcMod) {
+
+                    case 1:
+                        System.out.println("Ingrese el nuevo nombre: ");
+                        String nuevoNombre = sc.nextLine();
+                        habActual.setNombre(nuevoNombre);
+                        System.out.println("Nombre modificado.");
+                        break;
+
+                    case 2:
+                        System.out.println("Ingrese la nueva planta: ");
+                        int nuevaPlanta = sc.nextInt();
+                        habActual.setPlanta(nuevaPlanta);
+                        System.out.println("Planta modificada.");
+                        break;
+
+                    case 3:
+                        System.out.println("Ingrese los nuevos metros cuadrados: ");
+                        int nuevosMetros = sc.nextInt();
+                        habActual.setMetros(nuevosMetros);
+                        System.out.println("Metros modificados.");
+                        break;
+
+                    case 4:
+                        System.out.println("Ingrese si tiene salida (Si/No): ");
+                        String tiene = sc.nextLine();
+
+                        if (tiene.equalsIgnoreCase("si")) {
+                            habActual.setTieneSalida(true);
+                            System.out.println("Se cambio tiene salida.");
+                        } else if (tiene.equalsIgnoreCase("no")) {
+                            habActual.setTieneSalida(false);
+                            System.out.println("Se cambio tiene salida.");
+                        } else {
+                            System.out.println("No se pudo determinar si tiene salida.");
+                        }
+
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        System.out.println("Opcion incorrecta.");
+                }
+
+            } while (opcMod != 0);
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    // MENU DESAFIOS
+    public static void menuDesafios(Scanner sc, ArbolAVL avl) {
+
+        int op;
+
+        do {
+
+            System.out.println("\n----- ABM DESAFIOS -----");
+            System.out.println("1. Alta");
+            System.out.println("2. Baja");
+            System.out.println("3. Modificacion");
+            System.out.println("0. Volver");
+            System.out.print("Opcion: ");
+
+            op = sc.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    altaDesafio(sc, avl);
+                    break;
+
+                case 2:
+                    bajaDesafio(sc, avl);
+                    break;
+
+                case 3:
+                    modificarDesafio(sc, avl);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta.");
+            }
+
+        } while (op != 0);
+
+    }
+
+    public static void altaDesafio(Scanner sc, ArbolAVL avl) {
+
+        int codigoHab;
+        int puntaje;
+        String nombre;
+        String tipo;
+
+        System.out.println("Ingrese el codigo de la habitacion del desafio: ");
+        codigoHab = sc.nextInt();
+
+        // busco la habitacion, el desafio se guarda dentro del avl propio de esa
+        // habitacion
+        Habitacion habActual = (Habitacion) avl.recuperar(new Habitacion(codigoHab));
+
+        if (habActual != null) {
+
+            System.out.println("Ingrese el puntaje que otorga el desafio: ");
+            puntaje = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Ingrese el nombre del desafio: ");
+            nombre = sc.nextLine();
+
+            System.out.println("Ingrese el tipo del desafio: ");
+            tipo = sc.nextLine();
+
+            Desafio nuevoDesafio = new Desafio(puntaje, nombre, tipo);
+
+            if (habActual.getDesafios().insertar(nuevoDesafio)) {
+                System.out.println("Desafio agregado exitosamente.");
+            } else {
+                System.out.println("Ya existe un desafio con ese puntaje en esta habitacion.");
+            }
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    public static void bajaDesafio(Scanner sc, ArbolAVL avl) {
+
+        int codigoHab;
+        int puntajeBuscar;
+
+        System.out.println("Ingrese el codigo de la habitacion del desafio: ");
+        codigoHab = sc.nextInt();
+
+        Habitacion habActual = (Habitacion) avl.recuperar(new Habitacion(codigoHab));
+
+        if (habActual != null) {
+
+            System.out.println("Ingrese el puntaje del desafio a eliminar: ");
+            puntajeBuscar = sc.nextInt();
+
+            Desafio desafioActual = (Desafio) habActual.getDesafios().recuperar(new Desafio(puntajeBuscar));
+
+            if (desafioActual != null) {
+                habActual.getDesafios().eliminar(desafioActual);
+                System.out.println("Desafio eliminado correctamente.");
+            } else {
+                System.out.println("El desafio no existe en esta habitacion.");
+            }
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    public static void modificarDesafio(Scanner sc, ArbolAVL avl) {
+
+        int codigoHab;
+        int puntajeBuscar;
+
+        System.out.println("Ingrese el codigo de la habitacion del desafio: ");
+        codigoHab = sc.nextInt();
+
+        Habitacion habActual = (Habitacion) avl.recuperar(new Habitacion(codigoHab));
+
+        if (habActual != null) {
+
+            System.out.println("Ingrese el puntaje del desafio a modificar: ");
+            puntajeBuscar = sc.nextInt();
+            sc.nextLine();
+
+            Desafio desafioActual = (Desafio) habActual.getDesafios().recuperar(new Desafio(puntajeBuscar));
+
+            if (desafioActual != null) {
+
+                int opcMod;
+
+                do {
+
+                    System.out.println("\n--- MODIFICAR DESAFIO ---");
+                    System.out.println("1. Modificar Nombre");
+                    System.out.println("2. Modificar Tipo");
+                    System.out.println("0. Volver");
+                    System.out.print("Opcion: ");
+
+                    opcMod = sc.nextInt();
+                    sc.nextLine();
+
+                    switch (opcMod) {
+
+                        case 1:
+                            System.out.println("Ingrese el nuevo nombre: ");
+                            String nuevoNombre = sc.nextLine();
+                            desafioActual.setNombre(nuevoNombre);
+                            System.out.println("Nombre modificado.");
+                            break;
+
+                        case 2:
+                            System.out.println("Ingrese el nuevo tipo: ");
+                            String nuevoTipo = sc.nextLine();
+                            desafioActual.setTipo(nuevoTipo);
+                            System.out.println("Tipo modificado.");
+                            break;
+
+                        case 0:
+                            break;
+
+                        default:
+                            System.out.println("Opcion incorrecta.");
+                    }
+
+                } while (opcMod != 0);
+
+            } else {
+                System.out.println("El desafio no existe en esta habitacion.");
+            }
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    // MENU EQUIPOS
+    public static void menuEquipos(Scanner sc, TablaHashEquipos tabla, ArbolAVL avl) {
+
+        int op;
+
+        do {
+
+            System.out.println("\n----- ABM EQUIPOS -----");
+            System.out.println("1. Alta");
+            System.out.println("2. Baja");
+            System.out.println("3. Modificacion");
+            System.out.println("0. Volver");
+            System.out.print("Opción: ");
+
+            op = sc.nextInt();
+
+            switch (op) {
+
+                case 1:
+                    altaEquipo(sc, tabla, avl);
+                    break;
+
+                case 2:
+                    bajaEquipo(sc, tabla);
+                    break;
+
+                case 3:
+                    modificarEquipo(sc, tabla, avl);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Opcion incorrecta.");
+            }
+
+        } while (op != 0);
+
+    }
+
+    public static void altaEquipo(Scanner sc, TablaHashEquipos tabla, ArbolAVL avl) {
+
+        String nombre;
+        int puntajeExigido;
+        int codigoHabInicial;
+
+        System.out.println("Ingrese el nombre del equipo: ");
+        nombre = sc.nextLine();
+
+        System.out.println("Ingrese el puntaje exigido para salir de la casa: ");
+        puntajeExigido = sc.nextInt();
+
+        System.out.println("Ingrese el codigo de la habitacion inicial del equipo: ");
+        codigoHabInicial = sc.nextInt();
+
+        // busco la habitacion donde arranca el equipo
+        Habitacion habInicial = (Habitacion) avl.recuperar(new Habitacion(codigoHabInicial));
+
+        if (habInicial != null) {
+
+            // arranca con puntaje acumulado 0 y puntaje actual en la habitacion 0
+            Equipo nuevoEquipo = new Equipo(nombre, puntajeExigido, 0, habInicial, 0);
+
+            if (tabla.insertar(nuevoEquipo)) {
+                System.out.println("Equipo agregado exitosamente.");
+            } else {
+                System.out.println("Ya existe un equipo con ese nombre.");
+            }
+
+        } else {
+            System.out.println("La habitacion no existe.");
+        }
+    }
+
+    public static void bajaEquipo(Scanner sc, TablaHashEquipos tabla) {
+
+        String nombreBuscar;
+
+        System.out.println("Ingrese el nombre del equipo a eliminar: ");
+        nombreBuscar = sc.nextLine();
+
+        Equipo equipoActual = (Equipo) tabla.buscar(nombreBuscar);
+
+        if (equipoActual != null) {
+            //tabla.eliminar(equipoActual);//hacer este metodo
+            System.out.println("Equipo eliminado correctamente.");
+        } else {
+            System.out.println("El equipo no existe.");
+        }
+    }
+
+    public static void modificarEquipo(Scanner sc, TablaHashEquipos tabla, ArbolAVL avl) {
+
+        String nombreBuscar;
+
+        System.out.println("Ingrese el nombre del equipo: ");
+        nombreBuscar = sc.nextLine();
+
+        Equipo equipoActual = (Equipo) tabla.buscar(nombreBuscar);
+
+        if (equipoActual != null) {
+
+            int opcMod;
+
+            do {
+
+                System.out.println("\n--- MODIFICAR EQUIPO ---");
+                System.out.println("1. Modificar Puntaje Exigido");
+                System.out.println("2. Modificar Habitacion Actual");
+                System.out.println("0. Volver");
+                System.out.print("Opcion: ");
+
+                opcMod = sc.nextInt();
+
+                switch (opcMod) {
+
+                    case 1:
+                        System.out.println("Ingrese el nuevo puntaje exigido: ");
+                        int nuevoPuntajeExigido = sc.nextInt();
+                        equipoActual.setPuntajeExigido(nuevoPuntajeExigido);
+                        System.out.println("Puntaje exigido modificado.");
+                        break;
+
+                    case 2:
+                        System.out.println("Ingrese el codigo de la nueva habitacion: ");
+                        int nuevoCodigoHab = sc.nextInt();
+
+                        Habitacion nuevaHab = (Habitacion) avl.recuperar(new Habitacion(nuevoCodigoHab));
+
+                        if (nuevaHab != null) {
+                            equipoActual.setHabitacionActual(nuevaHab);
+                            System.out.println("Habitacion actual modificada.");
+                        } else {
+                            System.out.println("La habitacion no existe.");
+                        }
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        System.out.println("Opcion incorrecta.");
+                }
+
+            } while (opcMod != 0);
+
+        } else {
+            System.out.println("El equipo no existe.");
+        }
+    }
+
+}
---
+++ Sistema/CargarArchivo.java
@@ -0,0 +1,123 @@
+package Sistema;
+import GrafoHabitaciones.Grafo;
+import GrafoHabitaciones.estructurasAuxiliares.ArbolAVL;
+import clases.Desafio;
+import clases.Equipo;
+import clases.Habitacion;
+import clases.TablaHashEquipos;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+public class CargarArchivo {
+
+
+    public static void cargarArchivo(ArbolAVL avlHabitaciones, Grafo grafo, TablaHashEquipos tablaEquipos, DesafiosResueltos desResueltos) {
+
+        String nombreArchivoEntrada = "Sistema.txt";// guardo el nombre del archivo en un string
+        // gestion de errores
+        try {
+            FileReader lectorArchivo = new FileReader(nombreArchivoEntrada); // leo archivos
+            BufferedReader lectura = new BufferedReader(lectorArchivo);
+            String linea = ""; // creo variable linea vacia, para leer cada linea del txt
+            while ((linea = lectura.readLine()) != null) { // se recorre mientras haya una linea
+                if(!linea.isEmpty()){
+                    String primerCaracter = linea.substring(0, 1);
+                    String[] bloque = linea.split(";"); // variable para leer cada cadena de linea y separarlo con el ;
+                    if (primerCaracter.equals("H")) {
+                        if (bloque.length == 6) { // valido si la cadena del bloque tiene todos los datos
+                            int codigo = Integer.parseInt(bloque[1]);// uso Integer.parseInt para cambiar el tipo de dato
+                            String nombre = bloque[2];
+                            int planta = Integer.parseInt(bloque[3]); // uso Integer.parseInt para cambiar el tipo de dato
+                            int metros = Integer.parseInt(bloque[4]); // uso Integer.parseInt para cambiar el tipo de dato
+                            boolean tieneSalida = Boolean.parseBoolean(bloque[5]); // uso Boolean.parseBoolean para cambiar
+                                                                                   // el tipo de dato
+    
+                            // Almaceno la habitacion
+                            avlHabitaciones.insertar(new Habitacion(codigo, nombre, planta, metros, tieneSalida));
+                            grafo.insertarHabitacion(codigo);
+                        }
+                    } else if (primerCaracter.equals("D")) {
+                        if (bloque.length == 5) {
+                            int puntaje = Integer.parseInt(bloque[1]);
+                            int codigoHab = Integer.parseInt(bloque[2]);
+                            String nombre = bloque[3];
+                            String tipo = bloque[4];
+                            //busco la habitacion
+                            Habitacion hab = (Habitacion) avlHabitaciones.recuperar(new Habitacion(codigoHab));//creo nueva habitacion para buscarla en el arbol con el compareTo. Como buscar devuelve un comparable, yo lo casteo a un Habitacion
+                            if (hab != null) {
+                                hab.getDesafios().insertar(new Desafio(puntaje, nombre, tipo));
+                            }
+                        }
+                    } else if (primerCaracter.equals("P")) {
+                        String siguiente = linea.substring(2); // saco P:
+                        String[] datosArco = siguiente.split(";");
+                        int codigoHab1 = Integer.parseInt(datosArco[0]);
+                        int codigoHab2 = Integer.parseInt(datosArco[1]);
+                        int puntajeExigido = Integer.parseInt(datosArco[2]);
+    
+                        grafo.insertarArco(codigoHab1, codigoHab2, puntajeExigido);
+                    } else if (primerCaracter.equals("E")) {
+                        if (bloque.length >= 6) {
+                            String nombreEquipo = bloque[1];
+                            int puntajeExigido = Integer.parseInt(bloque[2]);
+                            int puntajeAcumulado = Integer.parseInt(bloque[3]);
+                            int codigoHabActual = Integer.parseInt(bloque[4]);
+                            int puntajeActualHab = Integer.parseInt(bloque[5]);
+                            //busco la habitacion
+                            Habitacion habActual = (Habitacion) avlHabitaciones.recuperar(new Habitacion(codigoHabActual));//creo nueva habitacion para buscarla en el arbol con el compareTo. Como buscar devuelve un comparable, yo lo casteo a un Habitacion
+    
+                            Equipo nuevoEquipo = new Equipo(nombreEquipo, puntajeExigido, puntajeAcumulado, habActual, puntajeActualHab);
+                            tablaEquipos.insertar(nuevoEquipo);
+    
+                            if (bloque.length == 7) {
+                                String listaDesafios = bloque[6];
+                                int pos = 0; //pos marca desde donde sigo buscando el proximo "(" en cada vuelta del while
+                                while (listaDesafios.indexOf("(", pos) != -1) {
+    
+                                    int inicioParentesis = listaDesafios.indexOf("(", pos);
+                                    int finParentesis = listaDesafios.indexOf(")", inicioParentesis);
+    
+                                    String grupo = listaDesafios.substring(inicioParentesis + 1, finParentesis);//guardo el formato de 1:20,30 sin los paretenssus
+    
+                                    String[] partesGrupo = grupo.split(":");//primero esta el codigo de la habtacion y despues se separa con : para los puntajes de los desafios, por eso corto con :
+                                    int codigoHabDesafio = Integer.parseInt(partesGrupo[0]); //guardo el codigo de la habitacion
+    
+                                    String[] puntajes = partesGrupo[1].split(",");//los puntajes se separan por ,
+                                    //busco la habitacion
+                                    Habitacion habDesafio = (Habitacion) avlHabitaciones.recuperar(new Habitacion(codigoHabDesafio));////creo nueva habitacion para buscarla en el arbol con el compareTo. Como buscar devuelve un comparable, yo lo casteo a un Habitacion
+    
+                                    if (habDesafio != null) {
+                                        // recorro cada puntaje de la lista
+                                        for (int i = 0; i < puntajes.length; i++) {
+                                            int puntaje = Integer.parseInt(puntajes[i]);
+                                            //busco el desafio resuelto dentro de la habitacion
+                                            Desafio desafioResuelto = (Desafio) habDesafio.getDesafios().buscar(new Desafio(puntaje)); //hacer cuando terminen desafios
+    
+                                            if (desafioResuelto != null) {
+                                                desResueltos.agregar(nombreEquipo, desafioResuelto);
+                                            }
+                                        }
+                                    }
+    
+                                    pos = finParentesis + 1;//avanzo el pos, una posicion mas del ultimo parentesis
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            lectorArchivo.close();
+            lectura.close();
+            System.out.println("Carga Finalizada." );
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage() + "\nEl archivo no existe.");
+        } catch (IOException error) {
+            System.out.println("Error al leer el archivo " + error.getMessage());
+        }
+    }
+}
