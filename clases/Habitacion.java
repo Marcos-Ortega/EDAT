4ae4f30947598ac476c04f04460dcc27ec5f77e7
Guada <romero.guadalupe.epet20@gmail.com>
2026-07-26 22:08:33 -0300

    modifico clase habitacion

--- clases/Habitacion.java
+++ clases/Habitacion.java
@@ -1,5 +1,7 @@
 package clases;
+
 import GrafoHabitaciones.estructurasAuxiliares.*;
+
 public class Habitacion implements Comparable<Habitacion> {
     private int codigo;
     private String nombre;
@@ -8,10 +10,10 @@ public class Habitacion implements Comparable<Habitacion> {
     private boolean tieneSalida;
     private ArbolAVL desafios; // cada habitacion conoce sus desafios
 
-    //constructores
+    // constructores
     public Habitacion(int unCodigo) {
         this.codigo = unCodigo;
-        this.desafios = new ArbolAVL(); 
+        this.desafios = new ArbolAVL();
     }
 
     public Habitacion(int unCodigo, String nom, int unaPlanta, int losMetros, boolean siTieneSalida) {
@@ -20,25 +22,30 @@ public class Habitacion implements Comparable<Habitacion> {
         this.planta = unaPlanta;
         this.metros = losMetros;
         this.tieneSalida = siTieneSalida;
-        this.desafios = new ArbolAVL(); 
+        this.desafios = new ArbolAVL();
     }
 
     // visualizadores
     public int getCodigo() {
         return this.codigo;
     }
+
     public String getNombre() {
         return this.nombre;
     }
+
     public int getPlanta() {
         return this.planta;
     }
+
     public int getMetros() {
         return this.metros;
     }
+
     public boolean tieneSalida() {
         return this.tieneSalida;
     }
+
     public ArbolAVL getDesafios() {
         return this.desafios;
     }
@@ -47,29 +54,45 @@ public class Habitacion implements Comparable<Habitacion> {
     public void setNombre(String nom) {
         this.nombre = nom;
     }
+
     public void setPlanta(int unaPlanta) {
         this.planta = unaPlanta;
     }
+
     public void setMetros(int losMetros) {
         this.metros = losMetros;
     }
+
     public void setTieneSalida(boolean siTieneSalida) {
         this.tieneSalida = siTieneSalida;
     }
 
     @Override
-    public int compareTo(Habitacion otra) { 
+    public int compareTo(Habitacion otra) {
         return Integer.compare(this.codigo, otra.codigo);
     }
 
-    public boolean equals(Habitacion otra) {
-        return this.codigo==otra.codigo;
+    @Override
+    public boolean equals(Object otraHab) {
+        boolean iguales = false;
+        if (otraHab != null && otraHab instanceof Habitacion) {
+            Habitacion otra = (Habitacion) otraHab;
+            if (this.codigo == otra.codigo) {
+                iguales = true;
+            }
+        }
+        return iguales;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(this.codigo);
     }
 
     public String toString() {
         return "Habitacion [Codigo: " + codigo + ", Nombre: " + nombre + ", Planta: " + planta +
                 ", Metros: " + metros + ", Salida Exterior: " + (tieneSalida ? "Sí" : "No") + "]";
     }
-    //metodos de los desafios 
-    
+    // metodos de los desafios
+
 }
